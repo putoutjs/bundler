1a44a75cea648052b46ef245cd137e23ffb94db0
coderaiser <coderaiser@cloudcmd.io>
2024-02-19 18:14:39 +0200

    feature: @putout/bundler: add

---
+++ .madrun.js
@@ -0,0 +1,20 @@
+import {
+    run,
+    cutEnv,
+} from 'madrun';
+
+const env = {};
+
+export default {
+    'wisdom': () => run('lint'),
+    'test': () => [env, `tape 'lib/**/*.spec.js' test/*.js 'rules/**/*.spec.js'`],
+    'test:dts': () => 'check-dts test/*.ts',
+    'watch:test': async () => [env, `nodemon -w lib -w test -x ${await cutEnv('test')}`],
+    'lint': () => `putout .`,
+    'fresh:lint': () => run('lint', '--fresh'),
+    'lint:fresh': () => run('lint', '--fresh'),
+    'fix:lint': () => run('lint', '--fix'),
+    'coverage': async () => [env, `c8 ${await cutEnv('test')}`],
+    'coverage:html': async () => [env, `c8 --reporter=lcov ${await cutEnv('test')}`],
+    'report': () => 'c8 report --reporter=lcov',
+};
